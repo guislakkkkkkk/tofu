629420d78218a455a6264006bca21751e0d68083
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-08-25 12:37:51 -0300

    Add files via upload

--- assets/preload/stages/TofuBG.lua
+++ assets/preload/stages/TofuBG.lua
@@ -1,11 +1,11 @@
 function onCreate()
 	-- background shit
-	makeAnimatedLuaSprite('tofubg', 'tofubg', -600, -300);
-	luaSpriteAddAnimationByPrefix('tofubg', 'tofubg','TofuBG idle', 24, true);
+	makeAnimatedLuaSprite('TofuBG', 'TofuBG', -600, -300);
+	luaSpriteAddAnimationByPrefix('TofuBG', 'TofuBG','TofuBG idle', 24, true);
 	setScrollFactor('TofuBG', 0.9, 0.9);
 
 	
-	addLuaSprite('tofubg', false);
+	addLuaSprite('TofuBG', true);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
